3cf0ec3ed78df95aa2d37a7d976935bbe94f5dc0
zakarya hari <zakariahari42@gmail.com>
2026-02-01 23:10:33 +0100

    Création de la table etapes avec relations vers routes et gares

---
+++ database/migrations/2026_02_01_215146_create_etapes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etapes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ordre');
+            $table->time('heure_passage');
+            $table->foreignId('route_id')->constrained('routes')->onDelete('cascade');
+            $table->foreignId('gare_id')->constrained('gares')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etapes');
+    }
+};
