f2239a3682c5bbddf722b243b76a270cab240ca7
zakarya hari <zakariahari42@gmail.com>
2026-02-01 23:10:55 +0100

    Création de la table programmes pour les horaires des routes

---
+++ database/migrations/2026_02_01_220423_create_programmes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programmes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('route_id')->constrained('routes')->onDelete('cascade');
+            $table->string('jour_depart');
+            $table->time('heure_depart');
+            $table->time('heure_arrivee');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programmes');
+    }
+};
