fe318234e00a5fb9007b6684ec17de5548260c52
zakarya hari <zakariahari42@gmail.com>
2026-02-02 00:48:25 +0100

    Création du modèle Gare avec relations vers Ville et Route

---
+++ app/Models/Gare.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gare extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'adresse',
+        'ville_id',
+    ];
+
+    /**
+     * Get the ville that owns the gare.
+     */
+    public function ville()
+    {
+        return $this->belongsTo(Ville::class);
+    }
+
+    /**
+     * The routes that belong to the gare.
+     */
+    public function routes()
+    {
+        return $this->belongsToMany(Route::class, 'etapes');
+    }
+}
